765e8e330df3c7faa3cbba0332975bb2ae30ac63
pikimaru <pikajunglist@gmail.com>
2024-07-05 18:27:15 +0200

    second hero is almoast done

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55a7796254b755371cceb1cde366682b41abc4fd84da55569e8b1c879e274cea
-size 8245
+oid sha256:d237ae4ba5a6c05ddef2de5404ba59173d6a16d2c061033f49fa7324b9c1eb9c
+size 8752
